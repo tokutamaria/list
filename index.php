606fc50f0d73053bd53d74cb55b16f18c4902cc9
TAKUTO ARIMA <ta910arima@gmail.com>
2020-08-17 21:06:28 +0900

    完成

--- index.php
+++ index.php
@@ -16,15 +16,26 @@ if(isset($_POST['submit'])) {
 
     $dbh = null;
 
+
     unset($name);
-
-
-    // echo "<pre>";
-    // var_dump($_POST);
-    // echo "</pre>";
-
-
 }
+
+if(isset($_POST['method']) && ($_POST['method'] === 'put')) {
+    $id = $_POST["id"];
+    $id = htmlspecialchars($id,ENT_QUOTES);
+    $id =(int)$id;
+
+    $dbh = db_connect();
+
+    $sql = 'UPDATE tasks SET done = 1 WHERE id = ?';
+    $stmt = $dbh->prepare($sql);
+
+    $stmt->bindValue(1, $id, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $dbh =null;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -45,7 +56,7 @@ if(isset($_POST['submit'])) {
 <?php
 $dbh = db_connect();
 
-$sql = 'SELECT id, name, done FROM tasks ORDER BY id DESC';
+$sql = 'SELECT id, name FROM tasks WHERE done = 0 ORDER BY id DESC';
 $stmt = $dbh->prepare($sql);
 $stmt->execute();
 $dbh =null;
